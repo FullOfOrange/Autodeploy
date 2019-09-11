cd1dc6bfa1c0530ddb5cda61efeb99425185b717
JinHyoukYork <jdd04026@gmail.com>
2019-09-11 14:18:33 +0900

    update: Update README.md & send req.body to response
    README 파일을 일부 추가하고, request 바디를 response로 받아서 postman 등으로 일부 볼 수 있도록 변경함

--- deployserver/app.js
+++ deployserver/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const uuid = require('uuid');
 
+
 const app = express();
 const id = uuid.v4();
 const port = 20000;
 
+app.use(express.json());
 app.get('/', function(req,res) {
-	res.send(id);
+	res.send(req.body);
 });
 app.listen(port, function() {
 	console.log('ex-app listen port : '+ port);
