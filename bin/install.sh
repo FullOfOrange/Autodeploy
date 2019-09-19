10ec41971f7a46f74607f2b4db070fff11a8e7fe
FullOfOrange <foorgdev@gmail.com>
2019-09-19 17:12:48 +0900

    fix: delete 'sudo' in install.sh 수도 명령어가 안먹어서 명령어를 제거 후 다시 실행하도록 변경함

--- bin/install.sh
+++ bin/install.sh
@@ -1,13 +1,14 @@
 echo "Install nginx"
-sudo cp ./nginx.repo /etc/yum.repos.d/
+cp ./nginx.repo /etc/yum.repos.d/
 
-sudo yum install -y nginx
 
-sudo firewall-cmd --permanent --zone=public --add-service=http
-sudo firewall-cmd --permanent --zone=public --add-service=https
-sudo firewall-cmd --reload
+yum install -y nginx
 
-sudo  cp ./nginx.conf /etc/nginx/
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
+
+cp ./nginx.conf /etc/nginx/
 
 systemctl start nginx
 systemctl enable nginx
@@ -16,18 +17,18 @@ echo "Install nodejs 10.x"
 
 curl -sL https://rpm.nodesource.com/setup_10.x | bash -
 
-sudo yum clean all && sudo yum makecache fast
-sudo yum install -y gcc-c++ make
-sudo yum install -y nodejs
+yum clean all && sudo yum makecache fast
+yum install -y gcc-c++ make
+yum install -y nodejs
 
 node -v
 
 echo "Install Docker"
 
-sudo yum install -y yum-utils \
+yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
 
-sudo yum install docker-ce docker-ce-cli containerd.io
+yum install -y docker-ce docker-ce-cli containerd.io
 
-sudo systemctl start docker
+systemctl start docker
