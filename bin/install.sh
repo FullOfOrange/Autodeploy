8682eb6927dd534e4338ccfb2ebf77a28d168fc8
FullOfOrange <foorgdev@gmail.com>
2019-09-20 00:24:24 +0900

    feat: add Centos install script centos 와 ubuntu를 구분할 수 있는 곳과 그에 맞는 함수를 실행 할 수 있도록 만들었다. 단 아직 우분투는 미지원함.

--- bin/install.sh
+++ bin/install.sh
@@ -1,37 +1,56 @@
-echo "Install nginx"
-cp ./nginx.repo /etc/yum.repos.d/
+centosInstall(){
+	echo "Install nginx"
+	cp ./nginx.repo /etc/yum.repos.d/
 
-yum install -y nginx
+	yum install -y nginx
 
-firewall-cmd --permanent --zone=public --add-service=http
-firewall-cmd --permanent --zone=public --add-service=https
-firewall-cmd --reload
+	firewall-cmd --permanent --zone=public --add-service=http
+	firewall-cmd --permanent --zone=public --add-service=https
+	firewall-cmd --reload
 
-cp ./nginx.conf /etc/nginx/
+	cp ./nginx.conf /etc/nginx/
 
-systemctl start nginx
-systemctl enable nginx
+	systemctl start nginx
+	systemctl enable nginx
 
-echo "Install nodejs 10.x"
+	echo "Install nodejs 10.x"
 
-curl -sL https://rpm.nodesource.com/setup_10.x | bash -
+	curl -sL https://rpm.nodesource.com/setup_10.x | bash -
 
-yum clean all && sudo yum makecache fast
-yum install -y gcc-c++ make
-yum install -y nodejs
+	yum clean all && sudo yum makecache fast
+	yum install -y gcc-c++ make
+	yum install -y nodejs
 
-node -v
+	node -v
 
-echo "Install Docker"
+	echo "Install Docker"
 
-yum install -y yum-utils \
-  device-mapper-persistent-data \
-  lvm2
+	yum install -y yum-utils \
+	  device-mapper-persistent-data \
+  	lvm2
 
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+	yum-config-manager \
+	  --add-repo \
+	  https://download.docker.com/linux/centos/docker-ce.repo
 
-yum install -y docker-ce docker-ce-cli containerd.io
+	yum install -y docker-ce docker-ce-cli containerd.io
 
-systemctl start docker
+	systemctl start docker
+
+}
+
+ubuntuInstall(){
+	echo "Not available"
+}
+
+OS=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+NAME="\"CentOS Linux\""
+echo $NAME
+
+if [ "$OS" = "$NAME" ]; then
+        echo "CENTOS"
+	centosInstall
+else
+        echo "Ubuntu"
+	ubuntuInstall
+fi
